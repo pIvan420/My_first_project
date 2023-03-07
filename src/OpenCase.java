e4917e429e92e811aaf8d18e5e36218d308ed2f6
Stepan Shmyak <iv-torsunow@yandex.ru>
2023-03-07 14:14:25 +0300

    исправил ошибку, когда вводил вместо числа строку, при первом вкладе в счет и все ломалось начал писать проверку инвентаря и продажу скинов из него (чот все так стремно идет пока, ну да пох, я допишу ету залупу и буду потом учить норм десктопку, ато рял залупа)

--- src/OpenCase.java
+++ src/OpenCase.java
@@ -4,13 +4,14 @@ import java.util.Scanner;
 // тут интерфейс консольный для открытия
 public class OpenCase {
     public static void start(){
-        Scanner scanner = new Scanner(System.in);
+        Scanner scanner;
         Inventory inventory;
         Case spectrum = new Spectrum();
         System.out.print("Привет, это открытие кейса \"Спектр\" из Cs:Go\nСколько денег вы положите на счет?\nВведите сумму: ");
         while (true){ // кладем деньги на счет
             try{ // защита от дурака на случай ввода строки или отрицательного числа
-                if (!scanner.hasNextFloat()) throw new IllegalArgumentException("Ошибка ввода, введите сумму еще" + // тут какая то ошибка
+                scanner = new Scanner(System.in);
+                if (!scanner.hasNextFloat()) throw new IllegalArgumentException("Ошибка ввода, введите сумму еще" +
                         " раз (Пример: 123.45)\nВведите сумму: ");
                 float curScanner = scanner.nextFloat();
                 if (curScanner <= 0) throw new IllegalArgumentException("Ошибка ввода, сумма не может быть" +
@@ -26,7 +27,7 @@ public class OpenCase {
             if (inventory.getSum() >= inventory.getKeyPrice()) { // проверка, хватает ли денег
                 System.out.println("Открываем? (Варианты ответа: Да, Нет)");
                 String response = scanner.next();
-                if (response.equals("Да")) { // если да, то работаем с OpenCase
+                if (response.equalsIgnoreCase("да")) { // если да, то работаем с OpenCase
                     try {
                         System.out.println("Открываем!");
                         Thread.sleep(1000);
@@ -39,16 +40,41 @@ public class OpenCase {
                         throw new RuntimeException(e);
                     }
 
-                } else if (response.equals("Нет")) { // тут надо прописать продажу скина и если не хочет, то выход из программы
-                    System.out.println("Тогда до встречи! :)");
-                    break;
+                } else if (response.equalsIgnoreCase("нет")) { // тут надо прописать продажу скина и если не хочет, то выход из программы
+                    boolean answer = checkInventory(inventory);
+                    System.out.println("Хотите ли продолжить открывать кейсы?");
+                    // тут еще логику прописать
                 } else {
                     System.out.println("Извините, я не понял, что вы написали. Будем открывать кейс? (Варианты ответа: Да, Нет)");
                 }
             }
             else{
-                System.out.println("Извините, но вам не хватает на открытие кейса. Удачи!"); // написать, если не хватает денег, то можно продавать скины из инвентаря
-                break;
+                System.out.println("Извините, но вам не хватает на открытие кейса. Ваш баланс: " + inventory.getSum()); // написать, если не хватает денег, то можно продавать скины из инвентаря
+                boolean answer = checkInventory(inventory);
+                if (answer) {
+                    System.out.println("Хорошо, продолжаем");
+                }
+                else {
+                    System.out.println("Спасибо что открывали кейс. Удачи!");
+                    break;
+                }
+            }
+        }
+    }
+
+    // добавить бы еще проверку на то, что у чела нет денег и нет оружия в инвентаре и сразу выход из игры в случае чего
+    private static boolean checkInventory(Inventory inventory){ // тут надо прописать проверку инвентаря и продажу скинов или еще как то, я пока не продумал
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            System.out.println("Хотите ли посмотреть инвентарь и продать ненужные скины? (Варианты ответа: Да, Нет)");
+            String strAnswer = scanner.next();
+            if (strAnswer.equalsIgnoreCase("да")) {
+                System.out.println("тут чота дописать бы");
+                return true;
+            } else if (strAnswer.equalsIgnoreCase("нет")) {
+                return false;
+            } else {
+                System.out.println("Извините, я не понял, что вы написали. Повторите ответ.");
             }
         }
     }
